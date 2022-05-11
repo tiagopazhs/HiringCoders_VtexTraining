84fffeac22f7fdfe1d6f3a6f679cb8f86764d9da
tiagopazhs <tiagohebert22@gmail.com>
2022-05-10 21:11:29 -0300

    week1

---
+++ Week-1/ProgrammingLogic/Example.py
@@ -0,0 +1,10 @@
+def FunctionPotency(a):
+    b = 2
+    while(b <= a):
+        print(str(b)+"ˆ2 = "+str(b*b))
+        b = b + 2
+    pass
+
+
+FunctionPotency(int(input("write a odd number: ")))
+    
