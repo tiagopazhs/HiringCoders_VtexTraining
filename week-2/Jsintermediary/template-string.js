9f775401343517a7f339d399c73dc65c441e7b49
tiagopazhs <tiagohebert22@gmail.com>
2022-05-12 18:58:43 -0300

    FirtsC

---
+++ week-2/Jsintermediary/template-string.js
@@ -0,0 +1,11 @@
+let name = 'Tiago';
+let lasName = 'Silva';
+myJob = 'Logistic';
+
+//without template string:
+console.log('Hi, I am ' + name + ' and my laste name is ' + lasName + ' and I work with ' + myJob);
+
+//with template string:
+console.log(`Hi, I am ${name} and my laste name is ${lasName} and I work with ${myJob}`);
+
+console.log(`the sum of 1 + 1 is ${1 + 1}`);
