337a1f8d9d0c3cdadcf097e7ed2e92f122d99204
tiagopazhs <tiagohebert22@gmail.com>
2022-05-18 05:42:08 -0300

    tSBuild1

---
+++ week-2/typeScript_/src/1-basic-types/strings.ts
@@ -0,0 +1,11 @@
+let nome: string;
+
+// ...
+
+nome = 'gabriel';
+
+function fullName(name: string, lastName: string){
+    return `${name} ${lastName}`
+}
+
+fullName('Gabriel', 'Ramos')
--- week-2/typeScript_/src/exemplo.ts
+++
@@ -1,6 +0,0 @@
-function soma(a, b) {
-    return a + b;
-}
-
-console.log(soma(1, 1));
-console.log(soma('1', '1'));
