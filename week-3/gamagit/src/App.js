e1b8b138fbbc6c5af6ddc298e80d30bc87817cf4
tiagopazhs <tiagohebert22@gmail.com>
2022-05-22 08:47:26 -0300

    lClass

---
+++ week-3/gamagit/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+    function App(props) {
+        return ( 
+        <>
+            <input name="User" id = "User" className = "UserInput" placeholder = "User"/>
+        </>);
+    }
+
+    export default App;
