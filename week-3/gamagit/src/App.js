0348b87190dca335e0bcf5ec31d25db72f112e6b
tiagopazhs <tiagohebert22@gmail.com>
2022-05-23 06:01:36 -0300

    fCW

--- week-3/gamagit/src/App.js
+++ week-3/gamagit/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from  'react';
 
     function App(props) {
         return ( 
         <>
             <input name="User" id = "User" className = "UserInput" placeholder = "User"/>
+            <button type="button">Search</button>
         </>);
     }
 
