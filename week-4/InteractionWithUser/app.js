e2f6caaa85c51eae835808a8f8bcb5c6549b29c6
tiagopazhs <tiagohebert22@gmail.com>
2022-06-24 09:30:40 -0300

    Changes

--- week-4/InteractionWithUser/app.js
+++ week-4/InteractionWithUser/app.js
@@ -25,6 +25,10 @@ const server = http.createServer((req, res) => {
     fs.writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err) {
       if (err) throw err;
       console.log('Saved!');
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(answer);
     });
 
     answer = 'User created with success :)'
@@ -33,16 +37,25 @@ const server = http.createServer((req, res) => {
   else if(urlparse.pathname == '/selecionar-usuario'){
     fs.readFile('users/' + params.id + '.txt', function (err, data) {
       answer = data;
-      console.log(answer);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(answer);
+
     })
-  };
+  }
 
 //Delete User
+  else if(urlparse.pathname == '/remover-usuario'){
+  fs.unlink('users/' + params.id + '.txt', function (err, data) {
+    answer = data;
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(answer);
 
-res.statusCode = 200;
-  res.setHeader('Content-Type', 'te.
-  xt/plain');
-  res.end(answer);
+  });
+}
+
 });
 
 //execucao
